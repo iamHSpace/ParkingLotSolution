7083b54846f9ea131d329963b4c493879e3f2072
iamhspace <sutharhimanshu98@gmail.com>
2022-05-28 18:47:36 +0530

    code comments.

--- src/main/java/com/code/parking/Service/LeaveService.java
+++ src/main/java/com/code/parking/Service/LeaveService.java
@@ -1,20 +1,19 @@
 package com.code.parking.Service;
 
-import java.util.Collection;
 import java.util.Collections;
 
 public class LeaveService extends ParkingLotConfig{
     @Override
     public void process(String[] instructionArray) throws Exception {
-        if(!parking_lot.isEmpty() && parking_lot != null){
-            Integer slotNumber = Integer.parseInt(instructionArray[1]);
-            if(slotNumber>=total_slots_alloted){
+        if(parking_lot != null && !parking_lot.isEmpty()){  //check if parking_lot exists and if parkinglot id empty or not
+            Integer slotNumber = Integer.parseInt(instructionArray[1]);  //get slot number from input instructionArray
+            if(slotNumber>=total_slots_alloted){   // if slotNumber is more than total parking slots, then tell user that slot number does not exist.
                 System.out.println("Slot number : " + slotNumber + " does not exist.");
             }else {
-                parking_lot.remove(slotNumber);
+                parking_lot.remove(slotNumber); // remove slotNumber from parking lot, hence marking it empty.
                 System.out.println("Slot number : " + slotNumber + " is free.");
-                slots_available.add(slotNumber);
-                Collections.sort(slots_available);
+                slots_available.add(slotNumber);  // add slotNumber to available slots, hence marking it available.
+                Collections.sort(slots_available); // sort, so that user gets first available slot while filling filling.
             }
         }else {
             System.out.println("Parking lot is already empty");
